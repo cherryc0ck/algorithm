544f20d47b0095fa5c0afadc5be7fa044b6e37e8
cherryc0ck <qstar9537@naver.com>
2023-03-05 16:49:03 +0900

    Add: Lv.0

--- programers/202303/삼각형의 완성조건.js
+++ programers/202303/삼각형의 완성조건.js
@@ -22,3 +22,18 @@ const solution = (sides) => {
 
   return sum > Math.max(...sides) ? 1 : 2;
 };
+// 다른사람 풀이
+function solution1(sides) {
+  sides = sides.sort((a, b) => a - b);
+  return sides[0] + sides[1] > sides[2] ? 1 : 2;
+}
+
+// 다른사람 풀이
+const solution2 = (slides) =>
+  slides.every((v, i, arr) => {
+    const hab = arr.reduce((acc, cur, idx) => (idx !== i ? acc + cur : acc), 0);
+
+    return v < hab;
+  })
+    ? 1
+    : 2;
