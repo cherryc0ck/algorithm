f029e968f9cd84a9aabeccc968b2bf4cf689c994
cherryc0ck <qstar9537@naver.com>
2023-03-07 12:02:52 +0900

    Add: Lv.0

--- programers/202303/최댓값만들기.js
+++ programers/202303/최댓값만들기.js
@@ -17,3 +17,9 @@ const solution = (nums) => {
   nums.sort((a, b) => b - a);
   return nums[0] * nums[1];
 };
+
+// 다른 사람 풀이
+function solution(numbers) {
+  let [a, b] = numbers.sort((a, b) => b - a);
+  return a * b;
+}
