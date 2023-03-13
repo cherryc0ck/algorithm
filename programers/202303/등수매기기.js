831cef50465d81770c2400351ac875b477881087
cherryc0ck <qstar9537@naver.com>
2023-03-13 10:50:35 +0900

    Add: Lv.0

--- programers/202303/등수매기기.js
+++ programers/202303/등수매기기.js
@@ -13,3 +13,40 @@
 // score	result
 // [[80, 70], [90, 50], [40, 70], [50, 80]]	[1, 2, 4, 3]
 // [[80, 70], [70, 80], [30, 50], [90, 100], [100, 90], [100, 100], [10, 30]]	[4, 4, 6, 2, 2, 1, 7]
+
+// 내 풀이
+const solution = (score) => {
+  let answer = [];
+  let avg = score.map((v) => (v[0] + v[1]) / 2);
+
+  for (let i = 0; i < avg.length; i++) {
+    let ranking = 1;
+
+    for (let j = 0; j < avg.length; j++) {
+      if (avg[i] < avg[j]) {
+        ranking++;
+      } else {
+        continue;
+      }
+    }
+    answer.push(ranking);
+    ranking = 0;
+  }
+  return answer;
+};
+
+// 다른 사람 풀이 1
+function solution(score) {
+  return score.map((el) => {
+    return (
+      score.filter((v) => (v[0] + v[1]) / 2 > (el[0] + el[1]) / 2).length + 1
+    );
+  });
+}
+
+// 다른 사람 풀이 2
+function solution(score) {
+  let avg = score.map((v) => (v[0] + v[1]) / 2);
+  let sorted = avg.slice().sort((a, b) => b - a);
+  return avg.map((v) => sorted.indexOf(v) + 1);
+}
