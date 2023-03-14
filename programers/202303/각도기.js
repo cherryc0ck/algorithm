76b92df6e6c18be58dcc021ee839de49586377d9
cherryc0ck <qstar9537@naver.com>
2023-03-14 12:27:40 +0900

    Chore: clean code

--- programers/202303/각도기.js
+++ programers/202303/각도기.js
@@ -22,3 +22,8 @@
 const solution = (angle) => {
   return angle >= 90 ? (angle === 90 ? 2 : angle === 180 ? 4 : 3) : 1;
 };
+
+// 다른사람 풀이
+function solution(angle) {
+  return [0, 90, 91, 180].filter((x) => angle >= x).length;
+}
