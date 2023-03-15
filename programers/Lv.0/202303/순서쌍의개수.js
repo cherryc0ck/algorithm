b705d094e0d601511eb3858368c1f99effffcbc3
cherryc0ck <qstar9537@naver.com>
2023-03-15 11:48:55 +0900

    Add: Lv.0

--- programers/Lv.0/202303/순서쌍의개수.js
+++ programers/Lv.0/202303/순서쌍의개수.js
@@ -10,3 +10,22 @@
 // n	result
 // 20	6
 // 100	9
+
+// 내 풀이
+const solution = (n) => {
+  let i = 1;
+  let answer = 0;
+  while (i <= n) {
+    n % i === 0 && (answer += 1);
+    i++;
+  }
+  return answer;
+};
+
+// 다른 사람 풀이
+function solution(n) {
+  return Array(n)
+    .fill(1)
+    .map((v, idx) => v + idx)
+    .filter((v) => n % v === 0).length;
+}
