f7e52e6cc9c1906820fc679f12d360c5b9b23113
cherryc0ck <qstar9537@naver.com>
2023-03-23 13:15:50 +0900

    unSolved: 구슬을나누는경우의수

--- programers/Lv.0/unSolved/구슬을나누는경우의수.js
+++ programers/Lv.0/unSolved/구슬을나누는경우의수.js
@@ -13,3 +13,31 @@
 // balls	share	result
 // 3	2	3
 // 5	3	10
+
+// 다른사람의 풀이1
+const 팩토리얼 = (num) => (num === 0 ? 1 : num * 팩토리얼(num - 1));
+
+function solution(balls, share) {
+  return Math.round(
+    팩토리얼(balls) / 팩토리얼(balls - share) / 팩토리얼(share)
+  );
+}
+
+// 다른사람의 풀이2
+const solution(balls, share) {
+  var result = 1;
+  while (share > 0) {
+    result = (result * balls) / share;
+    balls = balls - 1;
+    share = share - 1;
+  }
+  return Math.round(result);
+}
+
+// 다른사람의 풀이3
+function solution(balls, share) {
+  return (
+    Array.from({ length: share }, (_, i) => balls - i).reduce((a, b) => a * b) /
+    Array.from({ length: share }, (_, i) => share - i).reduce((a, b) => a * b)
+  );
+}
