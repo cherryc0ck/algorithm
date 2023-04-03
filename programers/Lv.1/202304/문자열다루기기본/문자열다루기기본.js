f2f2d9e89a7e1bf04e6caffa26e05489ec8d463d
cherryc0ck <qstar9537@naver.com>
2023-04-03 09:43:14 +0900

    add: [Lv.1] 약수의합

--- programers/Lv.1/202304/문자열다루기기본/문자열다루기기본.js
+++ programers/Lv.1/202304/문자열다루기기본/문자열다루기기본.js
@@ -1,3 +1,4 @@
+// 내 풀이
 const solution = (s) => {
   return ([4, 6].includes(s.length) && [...s].every((el) => el >= 0)) || false;
 };
---
+++ programers/Lv.1/202304/약수의합/약수의합.js
@@ -0,0 +1,6 @@
+const solution = (n) => {
+  let answer = 0;
+  for (let i = 1; i <= n; i++) answer += n % i === 0 && i;
+
+  return answer;
+};
