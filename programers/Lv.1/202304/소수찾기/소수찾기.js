18600786f2d6628f374e0d84dfcb0f7193c9989a
cherryc0ck <qstar9537@naver.com>
2023-04-04 12:43:54 +0900

    add: [Lv.1] 소수찾기

---
+++ programers/Lv.1/202304/소수찾기/소수찾기.js
@@ -0,0 +1,33 @@
+// 내 풀이
+const solution = (n) => {
+  let arr = Array(n + 1).fill(true);
+  arr[0] = false;
+  arr[1] = false;
+
+  for (let i = 2; i * i <= n; i++) {
+    if (arr[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        arr[j] = false;
+      }
+    }
+  }
+  return arr.filter((el) => el).length;
+};
+
+// 다른 사람 풀이
+function solution(n) {
+  const s = new Set();
+  for (let i = 1; i <= n; i += 2) {
+    s.add(i);
+  }
+  s.delete(1);
+  s.add(2);
+  for (let j = 3; j < Math.sqrt(n); j++) {
+    if (s.has(j)) {
+      for (let k = j * 2; k <= n; k += j) {
+        s.delete(k);
+      }
+    }
+  }
+  return s.size;
+}
