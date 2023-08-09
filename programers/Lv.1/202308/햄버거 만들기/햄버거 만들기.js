ba4b03bb3d1ff27d927af71c27e8523521f3331e
cherryc0ck <qstar9537@naver.com>
2023-08-09 11:23:24 +0900

    add: [Lv.1] 햄버거 만들기

---
+++ programers/Lv.1/202308/햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,21 @@
+// 내 풀이
+const solution = (ingredient) => {
+  const stk = [];
+  let cnt = 0;
+  ingredient.forEach((ing, idx) => {
+    stk.push(ing);
+
+    if (stk.length >= 4) {
+      const find = stk.slice(-4).join("");
+      if (find === "1231") {
+        stk.pop();
+        stk.pop();
+        stk.pop();
+        stk.pop();
+        cnt++;
+      }
+    }
+  });
+
+  return cnt;
+};
